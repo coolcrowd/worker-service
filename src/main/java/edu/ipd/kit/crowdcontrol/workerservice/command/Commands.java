842ddde9a9d785efc5a72b558ecc721fb5e48cc8
Leander Kurscheidt <Leander.Kurscheidt@gmx.de>
2016-01-06 17:03:46 +0100

    added javadoc

--- src/main/java/edu/ipd/kit/crowdcontrol/workerservice/command/Commands.java
+++ src/main/java/edu/ipd/kit/crowdcontrol/workerservice/command/Commands.java
@@ -2,6 +2,7 @@ package edu.ipd.kit.crowdcontrol.workerservice.command;
 
 import edu.ipd.kit.crowdcontrol.workerservice.RequestHelper;
 import edu.ipd.kit.crowdcontrol.workerservice.objectservice.Communication;
+import edu.ipd.kit.crowdcontrol.workerservice.proto.Emailanswer;
 import spark.Request;
 import spark.Response;
 
@@ -22,40 +23,46 @@ public class Commands implements RequestHelper {
     }
 
     /**
-     *
+     * The POST-command /email, where this methods corresponds to, persists the email for the specified worker or
+     * create a new if none specified.
+     * this method expects the JSON-Representation of the protobuf-message email in the body of the request.
+     * the response is the corresponding workerID for the email.
      * @param request  The request providing information about the HTTP request
      * @param response The response providing functionality for modifying the response
-     * @return
+     * @return an instance of Emailanswer
      */
-    public Object submitEmail(Request request, Response response) {
+    public Emailanswer submitEmail(Request request, Response response) {
         return null;
     }
 
     /**
-     *
+     * The POST-command /calibration, where this method corresponds to, persists the answer of an worker to the calibration querstions.
+     * this method expects the JSON-Representation of the protobuf-message calibration.
      * @param request  The request providing information about the HTTP request
      * @param response The response providing functionality for modifying the response
-     * @return
+     * @return emtpy body
      */
     public Object submitCalibration(Request request, Response response) {
         return null;
     }
 
     /**
-     *
+     * The POST-command /answer, where this method corresponds to, persists the answer of an worker to a creative-task.
+     * this method expects the JSON-Representation of the protobuf-message answer.
      * @param request  The request providing information about the HTTP request
      * @param response The response providing functionality for modifying the response
-     * @return
+     * @return emtpy body
      */
     public Object submitAnswer(Request request, Response response) {
         return null;
     }
 
     /**
-     *
+     * The POST-command /rating, where this method corresponds to, persists the rating of an answer.
+     * this method expects the JSON-Representation of the protobuf-message rating.
      * @param request  The request providing information about the HTTP request
      * @param response The response providing functionality for modifying the response
-     * @return
+     * @return emtpy body
      */
     public Object submitRating(Request request, Response response) {
         return null;
