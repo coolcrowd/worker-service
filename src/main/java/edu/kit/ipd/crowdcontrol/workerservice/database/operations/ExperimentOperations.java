8a4a7c5e9a9ca0a8041ff7a33079695084ad3330
Leander Kurscheidt <Leander.Kurscheidt@gmx.de>
2016-01-22 17:24:53 +0100

    database: updated operations for new schema and prepared AntiSpoof implementation

--- src/main/java/edu/kit/ipd/crowdcontrol/workerservice/database/operations/ExperimentOperations.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/workerservice/database/operations/ExperimentOperations.java
@@ -1,11 +1,13 @@
 package edu.kit.ipd.crowdcontrol.workerservice.database.operations;
 
 import edu.kit.ipd.crowdcontrol.workerservice.database.model.Tables;
+import edu.kit.ipd.crowdcontrol.workerservice.database.model.tables.records.AlgorithmTaskChooserParamRecord;
 import edu.kit.ipd.crowdcontrol.workerservice.database.model.tables.records.AlgorithmTaskChooserRecord;
 import edu.kit.ipd.crowdcontrol.workerservice.database.model.tables.records.ConstraintRecord;
 import edu.kit.ipd.crowdcontrol.workerservice.database.model.tables.records.ExperimentRecord;
 import org.jooq.DSLContext;
 import org.jooq.Result;
+import org.jooq.impl.DSL;
 
 import java.util.Map;
 
@@ -48,13 +50,51 @@ public class ExperimentOperations extends AbstractOperation {
     }
 
     /**
-     * if a record with the given name already exists, the method returns false, if not it inserts a row and returns true.
+     * If a record with the given name already exists, the method returns false, if not it inserts a row and returns true.
      * @param name the name of the TaskChooser
+     * @param description the description of the TaskChooser
      * @return true of inserted, false if already existing
      */
-    public boolean insertTaskChooserOrIgnore(String name) {
-        AlgorithmTaskChooserRecord record = new AlgorithmTaskChooserRecord(name);
+    public boolean insertTaskChooserOrIgnore(String name, String description) {
+        AlgorithmTaskChooserRecord record = new AlgorithmTaskChooserRecord(name, description);
         return create.executeInsert(record) == 1;
+    }
+
+    /**
+     * If a record with the passed parameters already exists, the method returns false, if not it inserts a row and
+     * returns true.
+     * @param taskChooser the referenced TaskChooser
+     * @param description the description of the parameter
+     * @param regex the regex the parameter mus be matching
+     * @param data the additional data the algorithm wants to save or null
+     * @return true if inserted, false if already existing
+     */
+    public boolean insertTaskChooserParamOrIgnore(String taskChooser, String description, String regex, String data) {
+        boolean exists = create.fetchExists(
+                DSL.selectFrom(Tables.ALGORITHM_TASK_CHOOSER_PARAM)
+                        .where(Tables.ALGORITHM_TASK_CHOOSER_PARAM.ALGORITHM.eq(taskChooser))
+                        .and(Tables.ALGORITHM_TASK_CHOOSER_PARAM.DESCRIPTION.eq(description))
+                        .and(Tables.ALGORITHM_TASK_CHOOSER_PARAM.REGEX.eq(regex))
+        );
+        if (!exists) {
+            return DSL.insertInto(Tables.ALGORITHM_TASK_CHOOSER_PARAM)
+                    .set(new AlgorithmTaskChooserParamRecord(null, description, regex, taskChooser, data))
+                    .execute() == 1;
+        }
+        return false;
+    }
+
+    /**
+     * returns the amount of answers submitted for the passed experiment
+     * @param experimentID the primary key of the experiment
+     * @return the number of answers submitted
+     */
+    public int getAnswersCount(int experimentID) {
+        //TODO duplicates?
+        return create.fetchCount(
+                DSL.selectFrom(Tables.ANSWER)
+                        .where(Tables.ANSWER.EXPERIMENT.eq(experimentID))
+        );
     }
 
     /**
@@ -63,10 +103,10 @@ public class ExperimentOperations extends AbstractOperation {
      * @return the map with the description as the key and the parameter-value as value
      */
     public Map<String, String> getTaskChooserParam(int experiment) {
-        return create.select(Tables.ALGORITHM_TASK_CHOOSER_PARAM.DESCRIPTION, Tables.CHOOSEN_TASK_CHOOSER_PARAM.VALUE)
+        return create.select(Tables.ALGORITHM_TASK_CHOOSER_PARAM.DATA, Tables.CHOOSEN_TASK_CHOOSER_PARAM.VALUE)
                 .from(Tables.CHOOSEN_TASK_CHOOSER_PARAM)
                 .join(Tables.ALGORITHM_TASK_CHOOSER_PARAM).onKey()
                 .where(Tables.CHOOSEN_TASK_CHOOSER_PARAM.EXPERIMENT.eq(experiment))
-                .fetchMap(Tables.ALGORITHM_TASK_CHOOSER_PARAM.DESCRIPTION, Tables.CHOOSEN_TASK_CHOOSER_PARAM.VALUE);
+                .fetchMap(Tables.ALGORITHM_TASK_CHOOSER_PARAM.DATA, Tables.CHOOSEN_TASK_CHOOSER_PARAM.VALUE);
     }
 }
