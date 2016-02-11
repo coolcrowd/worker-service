90334b9baa9928150b0d42ea225695d8ca1da3a4
Leander Kurscheidt <Leander.Kurscheidt@gmx.de>
2016-02-11 23:12:49 +0100

    docker: improved docker build

--- image/run.sh
+++ image/run.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-/opt/workerservice/bin/workerservice
+set -e
+
+# if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
+if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
+  exec /bin/bash -c "java $JAVA_OPTS -jar workerservice-all-0.5.jar $JENKINS_OPTS $@"
+fi
